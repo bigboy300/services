5ec3c5e8870ba1e3c8dad3ce5a424bc83fa7eb12
Pavel Khusnutdinov <afghye@mail.ru>
2023-12-13 02:15:07 +0300

    change 3.14

--- swiper.js
+++ swiper.js
@@ -43,4 +43,32 @@ if (window.innerWidth > 1120) {
   })
 }
 
-new Swiper('.swiper')
+const swiper = new Swiper('.mySwiper', {
+  //
+
+  direction: 'horizontal',
+  loop: true,
+  speed: 500,
+  effect: 'slider',
+  // initialSlide: 2, // Начинаем со 2 слайдера
+  // freeMode: true, // можно перетаскивать как ленту
+  slidesPerView: 3, // кол-во активных слайдов
+  // centeredSlides: true, // центрирование слайдов
+
+  //                 Пагинация (точки)
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true, // true - Пагинация становится кликабельной
+  },
+
+  //                   Кнопки вперед и назад
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  //  Автоматическое перелистывание
+  // autoplay: {
+  //     delay: 1000, //Задержка перед перелистыванием 1с = 1000мл/с
+  // },
+})
